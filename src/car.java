1193ad6309fde4a45bd08b8de79811f5c2d2c962
KumoCF <hiksunny16@gmail.com>
2025-04-15 22:13:12 -0300

    Atualizar o car.java

--- src/car.java
+++ src/car.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+public class Car {
+    public String brand;
+    public String model;
+    public int year;
 
-public class carro {
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + this.brand);
+        System.out.println("Model: " + this.model);
+        System.out.println("Year: " + this.year);
+    }
 }
