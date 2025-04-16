04137669f81190f6e1c00c27292d17084a81f542
KumoCF <hiksunny16@gmail.com>
2025-04-15 22:13:42 -0300

    Atualizar o retangulo.java

--- src/retangulo.java
+++ src/retangulo.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+public class Rectangle {
+    public double width;
+    public double height;
 
-public class ReEEEETANGuLO {
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return this.width * this.height;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (this.width + this.height);
+    }
 }
