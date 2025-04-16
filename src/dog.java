6e6cb0e5472c67e4ea8eaccaae00a786d7374268
KumoCF <hiksunny16@gmail.com>
2025-04-15 22:14:14 -0300

    Criar dog.java

---
+++ src/dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+    public String name;
+    public String race;
+    public int age;
+
+    public Dog(String name, String race, int age) {
+        this.name = name;
+        this.race = race;
+        this.age = age;
+    }
+
+    public void bark() {
+        System.out.println("WOLF");
+    }
+}
